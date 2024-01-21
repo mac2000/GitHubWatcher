83a01fd2e3f306901c16c8d156c96f57c76d39d3
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-01-21 19:07:45 +0200

    Initial Commit

---
+++ GitHubWatcher/GitHubWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubWatcherApp.swift
+//  GitHubWatcher
+//
+//  Created by Alexandr Marchenko on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
